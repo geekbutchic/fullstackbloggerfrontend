52eddaa13da5453fe0e68f0087d86d78f12e004a
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-07-06 23:43:08 -0600

    adds stying to blogs page for display

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -3,16 +3,28 @@ import React from "react";
 const BlogsPage = (props) => {
   return (
     <div className="blogs-page">
-      <h1>
-        Blogs Page
-      </h1>
+      <h1>Blogs Page</h1>
       <p>
-        Server Message: {props.allBlogs.map((blog) => {
-            return (
-                <>
-                {blog.title}
-                </>
-            )
+        Server Message:{" "}
+        {props.allBlogs.map((blog, index) => {
+          return (
+            <div
+              key={`${blog}-${index}`}
+              style={{
+                display: "flex",
+              }}
+            >
+              Id: {blog.id}
+              <br></br>  
+              Title : {blog.title.toUpperCase()}
+              <br></br>
+              Author : {blog.author}
+              <br></br>
+              Text: {blog.text}
+              <br></br>
+              {blog.createdAt}
+            </div>
+          );
         })}
       </p>
     </div>
