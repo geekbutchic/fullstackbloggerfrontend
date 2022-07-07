047219492443b367e55953cf4530da3c722d756d
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-07-07 13:29:08 -0600

    creates new states, passes states to blog page, creates new input fields, and passes state value to input field values, next step is on change handler for each input

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import { Routes, Route } from "react-router-dom";
 //react router imp
 import { useState, useEffect } from "react";
 //use state imp
-import BlogsPage from './Pages/Blogs'
+import BlogsPage from "./Pages/Blogs";
 import React from "react";
 import "./App.css";
 
 const urlEndpoint = "http://localhost:4000";
 
-const App = () => {
+const App = (props) => {
   const [serverJSON, setServerJSON] = useState([]);
+  const [sortField, setsSortField] = useState("");
+  const [sortOrder, setSortOrder] = useState("ASC");
+  const [filterField, setFilterField] = useState("");
+  const [filterValue, setFilterValue] = useState("");
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       const apiResponse = await fetch(`${urlEndpoint}/blogs/all-blogs`);
+      //endpoint modified to fetch all blogs
       const apiJSON = await apiResponse.json();
       console.log(apiJSON);
       setServerJSON(apiJSON);
@@ -25,7 +32,19 @@ const App = () => {
     <div className="app">
       <header className="app-header">
         <Routes>
-          <Route path="/blogs" element={<BlogsPage allBlogs={serverJSON}/>}/>
+          <Route
+            path="/blogs"
+            element=
+            {<BlogsPage 
+            serverJSON={serverJSON}
+            sortField={sortField} 
+            sortOrder={sortOrder}
+            filterField={filterField}
+            filterValue={filterValue}
+            limit={limit}
+            page={page}
+            />}
+          />
         </Routes>
       </header>
     </div>
--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -3,10 +3,57 @@ import React from "react";
 const BlogsPage = (props) => {
   return (
     <div className="blogs-page">
-      <h1>Blogs Page</h1>
+      <h1 style={{ display: "flex", justifyContent: "center"}}>Blogs Page</h1>
       <p>
-        Server Message:{" "}
-        {props.allBlogs.map((blog, index) => {
+        Select: {" "}
+        <select value={props.sortField}>
+            <option value="title">Title</option>
+            <option value="author">Author</option>
+            <option value="createdAt">Generated</option>
+        </select>
+        {" "}
+        SortOrder: {" "}
+        <select value={props.sortOrder}>
+            <option value="ASC">Ascend</option>
+            <option value="DESC">Descend</option>
+        </select>
+        {" "}
+        Filter Field: {" "}
+        <select value={props.filterField}>
+        <option value="title">Title</option>
+        <option value="author">Author</option>
+        </select>
+        <br></br>
+        Filter Value: {" "}
+        <input 
+        value={props.filterValue}
+        onChange={()=> {
+
+        }}
+        >
+        
+        </input>
+        {" "}
+        Limit: {" "}
+        <input
+        value={props.limit}
+        onChange={()=> {
+
+        }}
+        >
+        
+        </input>
+        {" "}
+        Page: {" "}
+        <input
+        value={props.page}
+        onChange={()=> {
+
+        }}
+        >
+        
+        </input>
+        {props.serverJSON.map((blog, index) => {
           return (
             <div
               key={`${blog}-${index}`}
